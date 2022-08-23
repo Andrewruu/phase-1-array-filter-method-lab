5107a7b0c0fe3af9b6e64d837b1af9b47ebb0d49
Andrew Wang <Andrewruujin@gmail.com>
2022-08-23 18:32:56 -0400

    commit tree 55777e5448323f096bb34f35c6190b2a0ea36148

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers,name){
+    const results = drivers.filter(people => people.toUpperCase() == name.toUpperCase())
+    return results;
+}
+function fuzzyMatch(drivers,letters){
+    const results = drivers.filter(people => people.startsWith(letters))
+    return results;
+}
+function matchName(drivers,names){
+    const results = drivers.filter(people => people.name == names)
+    return results;
+}
